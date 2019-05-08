29140798e4d56ddd942a84110bee18bfe21380b7
liujianping <liujianping.itech@qq.com>
2019-05-08 21:58:07 +0800

    update job.rb

--- job.rb
+++ job.rb
@@ -1,15 +1,13 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Job < Formula
   desc "JOB, make your short-term command as a long-term job"
   homepage "https://github.com/liujianping/job"
-  url "https://github.com/liujianping/job/releases/download/v0.1.1/job_0.1.1_Darwin_x86_64.tar.gz"
-  sha256 "8a3828f07d0ad5d3c727b22e3ef779810af7e059955f14493a1d6beee24f0e79"
+  url "https://github.com/liujianping/job/releases/download/v0.1.2/job_0.1.2_Darwin_x86_64.tar.gz"
+  sha256 "2105a9198461a3699a4904cf3dc6a97e6b5b3a54ee98fb1e4efbbd2b03a6e035"
   # depends_on "cmake" => :build
 
   def install
     # ENV.deparallelize  # if your formula fails when building in parallel
+    # Remove unrecognized options if warned by configure
     bin.install "job"
   end
 
