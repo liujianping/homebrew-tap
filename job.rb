04b44685f9841315c345a09ec5717e977e12dc5d
liujianping <jayl@yeetalk.net>
2019-05-12 21:58:44 +0800

    update job to v0.1.3

--- job.rb
+++ job.rb
@@ -1,14 +1,12 @@
 class Job < Formula
   desc "JOB, make your short-term command as a long-term job"
-  homepage "https://github.com/liujianping/job"
-  url "https://github.com/liujianping/job/releases/download/v0.1.2/job_0.1.2_Darwin_x86_64.tar.gz"
-  sha256 "2105a9198461a3699a4904cf3dc6a97e6b5b3a54ee98fb1e4efbbd2b03a6e035"
+  homepage ""
+  url "https://github.com/liujianping/job/releases/download/v0.1.3/job_0.1.3_Darwin_x86_64.tar.gz"
+  sha256 "978d145ab03b26edb93f05725149c5c725f19778dd2b449c185c4aa18bcda435"
   # depends_on "cmake" => :build
 
   def install
-    # ENV.deparallelize  # if your formula fails when building in parallel
-    # Remove unrecognized options if warned by configure
-    bin.install "job"
+    bin.install "job"		
   end
 
 end
