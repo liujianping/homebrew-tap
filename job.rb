7e0b230bee1216e6a7ef24b239ddc78601a6fd8f
liujianping <jayl@yeetalk.net>
2019-05-23 22:01:58 +0800

    update v0.1.4

--- job.rb
+++ job.rb
@@ -1,12 +1,11 @@
 class Job < Formula
   desc "JOB, make your short-term command as a long-term job"
-  homepage ""
-  url "https://github.com/liujianping/job/releases/download/v0.1.3/job_0.1.3_Darwin_x86_64.tar.gz"
-  sha256 "978d145ab03b26edb93f05725149c5c725f19778dd2b449c185c4aa18bcda435"
-  # depends_on "cmake" => :build
+  homepage "https://github.com/liujianping/job"
+  url "https://github.com/liujianping/job/releases/download/v0.1.4/job_0.1.4_Darwin_x86_64.tar.gz"
+  sha256 "5c9836426e71ba320f7b03c848f252692814fa50ec5b5b4808b556bf38a4b8a3"
 
   def install
-    bin.install "job"		
+	bin.install "job"
   end
 
 end
