66d6c270369310c159cacf57e3bfff6fcb810e89
JayL <liujianping.itech@qq.com>
2019-06-26 00:45:42 +0800

    update ts v0.0.3

--- ts.rb
+++ ts.rb
@@ -1,8 +1,8 @@
 class Ts < Formula
   desc "timestamp convert & compare tool"
   homepage "https://github.com/liujianping/ts"
-  url "https://github.com/liujianping/ts/releases/download/v0.0.2/ts_0.0.2_Darwin_x86_64.tar.gz"
-  sha256 "d43ad29cbb809cee0558326be79e0b00fb2e42e3ac279aa92307d581adb92fd5"
+  url "https://github.com/liujianping/ts/releases/download/v0.0.3/ts_0.0.3_Darwin_x86_64.tar.gz"
+  sha256 "cc2a24a7546cd07c05d5a2bb670f3c4192bd5093c7c33f81e855d57c4e5694dc"
 
   def install
 	bin.install "ts"
