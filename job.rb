0f02aa7cd2f85047bb6b8c23e8aeb15a5cb925ab
liujianping <liujianping.itech@qq.com>
2020-06-30 18:26:20 +0800

    update for job

--- job.rb
+++ job.rb
@@ -1,11 +1,14 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Job < Formula
-  desc "JOB, make your short-term command as a long-term job"
-  homepage "https://github.com/liujianping/job"
-  url "https://github.com/liujianping/job/releases/download/v0.1.4/job_0.1.4_Darwin_x86_64.tar.gz"
-  sha256 "5c9836426e71ba320f7b03c848f252692814fa50ec5b5b4808b556bf38a4b8a3"
+  desc "JOB, make your short-term command as a long-term job. 将命令行规划成任务的工具"
+  homepage ""
+  url "https://github.com/liujianping/job/releases/download/v0.2.0/job_0.2.0_Darwin_x86_64.tar.gz"
+  sha256 "b2a58d1af7aa4d87dd8991ac2a472c5ae36e93c2f461bbdb5e25c72f294c3220"
 
+  # depends_on "cmake" => :build
   def install
-	bin.install "job"
+    bin.install "job"
   end
-
 end
